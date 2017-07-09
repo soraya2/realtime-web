0df2536912732a7f278631dd5724cda50cb24ada
soraya2 <soraya_ibrahim@live.nl>
2017-07-09 23:30:59 +0200

    added offline and offline support
    and updated highscore (localstorage)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -14,11 +14,23 @@
     var form = document.getElementById('form');
     var userInput = document.getElementById('tweeter-list');
     var score = document.querySelector('.score > span');
+    var alertMessage = document.querySelector('.closebtn');
+    var message = document.querySelector('#message');
+    var messageText = document.querySelector('.message-text');
+    var highscore = document.querySelector('.highscore > span');
+    var birdLives = document.querySelectorAll('.lives > .bird-live');
+    var birdList = document.querySelector('.lives');
+    var tweetbox = document.getElementById('tweet');
     var counter = 0;
+    var lives = 2;
+    var tweet;
+
 
     if (score) {
         score.innerHTML = counter;
-        console.log(score);
+        // localStorage.highScore = '5';
+        highscore.innerHTML = localStorage.highScore;
+        // console.log(score);
     }
 
     var userValue;
@@ -29,29 +41,18 @@
         return id;
     }
 
+
     socket.on('time', function(data) {
         try {
             console.log(data.info.name);
 
-            userInput.addEventListener('change', function(event) {
-                // console.log(userInput.value);
-                userValue = userInput.value;
-
-                // event.preventDefault();
-                if (userInput.value !== '') {
-
-                    checkAnswer(data.info.name, userValue);
-                    // data.info.name === userInput.value
-                    // var input = userInput.value;
-                    // console.log(data.info.name === userInput.value, data.info.name);
-                }
-            });
             // var dataCheck = data.info.text;
             // var chatMessage = document.createElement('div');
             // var chat = document.getElementById('chat');
 
             sendData(data);
             optionData(data);
+            tweet = data.info.name;
 
 
             // chatMessage.classList.add('user-message');
@@ -72,7 +73,6 @@
 
     function sendData(data) {
         var i;
-        var tweetbox = document.getElementById('tweet');
         tweetbox.innerHTML = '';
         tweetbox.innerHTML = data.info.tweet;
 
@@ -97,36 +97,51 @@
     }
 
 
-    function checkAnswer(userAnswer, tweetName) {
-        // console.log(userAnswer, tweetName);
-        console.log(typeof counter);
+    userInput.addEventListener('change', function(event) {
+        // console.log(userInput.value);
+        userValue = userInput.value;
 
+        // event.preventDefault();
+        if (userInput.value !== '') {
+
+            checkAnswer(tweet, userValue);
+            // data.info.name === userInput.value
+            // var input = userInput.value;
+            // console.log(data.info.name === userInput.value, data.info.name);
+        }
+    });
+
+    function checkAnswer(userAnswer, tweetName) {
+
+        localStorage.highScore = '5';
         if (userAnswer === tweetName) {
 
-            // console.log('good');
-            // score.innerHTML
-            score.innerHTML = counter++;
+            counter++;
+            console.log('good');
+            score.innerHTML = counter;
+
 
         } else {
             console.log('fault');
+
+            console.log(birdLives);
+            if (lives !== -1) {
+                birdList.removeChild(birdLives[lives]);
+                lives--;
+
+            } else {
+                socket.removeAllListeners("time");
+                tweetbox.innerHTML = 'GAME OVER!';
+
+            }
         }
     }
-
-    // socket.on('connection', function(data) {
-    //     console.log('hello');
-
-    // });
-
-    // socket.on('connection', function(client) {
-    //     client.send("hello");
-    //     console.log("hello", client);
-    // });
 
 
     socket.on('connect', function(client) {
 
-        console.log('sjsjsj');
-        window.stop();
+        console.log('socket connected');
+
         socket.emit('login', { userId: socket.id });
     });
 
@@ -164,8 +179,6 @@
                 console.log('offline');
             }
 
-
-
             // console.log('reconnect');
 
         }
@@ -183,4 +196,64 @@
         reconncect = true;
 
     }
+
+    window.addEventListener('load', function(e) {
+        if (navigator.onLine) {
+            console.log("online");
+            // updateConnectionStatus('Online', true);
+        } else {
+            console.log("offline");
+            // updateConnectionStatus('Offline', false);
+        }
+    }, false);
+
+    window.addEventListener('online', function(e) {
+        console.log("And we're back :)");
+
+        message.classList.remove("hide");
+        message.classList.add("online");
+        messageText.innerHTML = 'Connected';
+        if (highscore) {
+
+            highscore.innerHTML = localStorage.highScore;
+        }
+        // message-text
+        setTimeout(function() {
+            message.classList.add("hide");
+            message.parentElement.classList.remove("online");
+
+        }, 3500);
+
+    }, false);
+
+    window.addEventListener('offline', function(e) {
+
+        console.log("offline.");
+        message.classList.remove("hide", "online");
+        message.classList.add("alert");
+        messageText.innerHTML = 'Disconnected';
+        if (typeof(Storage) !== "undefined") {
+            // Code for localStorage/sessionStorage.
+            // localStorage.highScore = score.innerHTML;
+
+            console.log(localStorage.highScore);
+            console.log(score.innerHTML);
+            socket.removeAllListeners("time");
+        } else {
+            // Sorry! No Web Storage support..
+        }
+
+    }, false);
+
+    alertMessage.addEventListener('click', function(e) {
+        this.parentElement.classList.toggle("hide");
+        // this.parentElement.classList.remove("alert");
+    });
+
+    console.log(alertMessage);
 })();
+
+// socket.broadcast.emit('users_count', clients);
+// io.sockets.on('users_count', function(client) {
+
+// });
