3f0f5ad976fddf9fadb715f5964eaac8b5d761d5
soraya2 <soraya_ibrahim@live.nl>
2017-07-12 12:06:11 +0200

    removed remains bcrypt

--- models/user.js
+++ models/user.js
@@ -21,16 +21,7 @@ var userSchema = mongoose.Schema({
     }
 });
 
-// Methods ======================
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
 
-// Checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
 
 // Create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
